69180e905ec77d640ffc147cc30b9236250dc942
Turing (Coder) <dev@uiuc-pickup.local>
2026-03-31 00:58:28 -0500

    wip: save current refactor progress

--- utils/request.js
+++ utils/request.js
@@ -9,7 +9,7 @@ function getBaseURL() {
     const accountInfo = wx.getAccountInfoSync();
     const envVersion = accountInfo.miniProgram.envVersion;
     if (envVersion === 'develop' || envVersion === 'trial') {
-      return wx.getStorageSync('baseURL') || 'http://localhost:9090/api/v1';
+      return wx.getStorageSync('baseURL') || 'https://api.zocpstudio.com/api/v1';
     }
   } catch (e) {
     // fallback to default
